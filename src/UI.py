7e100f1d6b905e95aec7df5c2b7faf7c350414d7
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-09-13 22:42:51 -0500

    Added option 3 which allows the user to Updatea column.

--- src/UI.py
+++ src/UI.py
@@ -1,6 +1,7 @@
 __author__ = 'Alex'
 from src.CreateDatabaseTable import Create
 from src.AddTableRow import AddRow
+from src.UpdateTableRow import UpdateRow
 class UI:
     def __init__(self):
         pass
@@ -9,6 +10,7 @@ class UI:
         # gets instance for the Create class
         database = Create
         add = AddRow
+        update = UpdateRow
 
         # variable to get file connection
         file = database.Create_Database(database)
@@ -37,9 +39,28 @@ class UI:
                 price = float(input("Enter in the Price: "))
                 description = str(input("Enter in a Product Description: "))
 
+                # sent information to class to add
                 add.row(add, cursor, connection, id, productID, productName, quantity, price, description)
 
+            if userOption == 3:
+                print("-----OPTION 3-------------------------------------------\n")
 
+                # variables and parameters to update a row in the Products table
+                columnUpdate = input("Which Table Column do you wish to update? ")
+                newInput = input("What would you like to change " + columnUpdate + " to? ")
+                viaRowID = input("Which row would you like to update via RowID? ")
+
+                # sends information to class to update
+                update.row(update, cursor, connection, columnUpdate, newInput, viaRowID)
+
+            if userOption == 4:
+                print("-----OPTION 4-------------------------------------------\n")
+
+                # variables and parameters to update a row in the Products table
+
+
+                # sends information to class to update
+                update.row(update, cursor, connection)
 
     def Options(self):
         option1 = "1): Create a new Database and Database Table"
--- src/UpdateTableRow.py
+++ src/UpdateTableRow.py
@@ -1,1 +1,23 @@
 __author__ = 'Alex'
+import sqlite3
+class UpdateRow():
+    def __init__(self):
+        pass
+
+    # updates a row via rowid
+    def row(self, cursor, connection, column, nInput, id):
+        try:
+            cursor.execute("UPDATE Products SET " + str(column) + "=('" + str(nInput) + "') WHERE RowID=" + str(id) + ";")
+
+            # if successful, print these statements and commit
+            print("----------------")
+            print("Update Success!")
+            print()
+            # commits changes to database
+            connection.commit()
+
+        #if unsuccessful, catch error and print these statements
+        except sqlite3.OperationalError:
+            print("------------------------------------------------------------------------")
+            print("Incorrect Input! Please check that your column name and ID is correct!")
+            print()
